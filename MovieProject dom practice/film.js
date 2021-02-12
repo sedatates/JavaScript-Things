5501d37046c82fcfd7d7bc679a104024493e1cce
Sedat <64736322+meftoone@users.noreply.github.com>
2021-02-12 21:11:31 +0300

    Add files via upload

---
+++ MovieProject dom practice/film.js
@@ -0,0 +1,15 @@
+/*function Film(title,director,url){
+    this.title = title;
+    this.director = director;
+    this.url = url;
+    
+}*/
+
+//ES6
+class Film {
+    constructor(title,director,url){
+        this.title=title;
+        this.director=director;
+        this.url=url;
+    }
+}
